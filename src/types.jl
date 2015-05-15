d3f77d0dad99c3e0ecdf7a797d18fd2d4875cd5c
ClaireV <ClaireVern@users.noreply.github.com>
2015-05-15 23:54:26 +0200

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -1,11 +1,3 @@
-# A single rating. The user and item are both represented by integer ids. A map
-# between ids and user/item names is stored elsewhere, both in the rating set
-# and the model.
-type Rating
-    user::Int32
-    item::Int32
-    value::Float32
-end
 
 # An internal type used only by the train function for caching between epochs.
 type Stats
